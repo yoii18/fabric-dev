871da4b29b733711492e1085c54957b94f60eb6e
Yoii <119809971+yoii18@users.noreply.github.com>
2026-08-21 08:44:44 +0530

    Committing 3 items from workspace e26a77bb-d4bf-4c6f-a871-c02f266e627c

---
+++ workspace_items/NBs/test.Notebook/notebook-content.py
@@ -0,0 +1,21 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+print("Hello World")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
