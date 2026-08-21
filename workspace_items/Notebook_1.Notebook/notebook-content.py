4a7281cf8847009d43af1d8a14f88e34c03c5cde
yoii18 <dpssalhotra@gmail.com>
2026-08-21 09:21:39 +0530

    added a cell

--- workspace_items/Notebook_1.Notebook/notebook-content.py
+++ workspace_items/Notebook_1.Notebook/notebook-content.py
@@ -21,3 +21,15 @@
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
 # META }
+
+
+# CELL ********************
+
+print("Hello World")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
